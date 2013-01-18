560956b06f207db377f152992dddd52dc6330df7
abelass <websolutions@mychacra.net>
2013-01-18 18:21:11 +0100

    le tyope lien

---
+++ lang/websimple_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/selection_d_objets/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'compris_pack' => 'Compris dans le pack',
+
+);
+?>
