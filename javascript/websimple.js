0f985fb627a319c3adbbfc4fcff1396a93ffb108
Rainer <abelass@Chuperlapup.(none)>
2013-02-07 11:55:43 +0100

    ranger les fenêtres eviter le bubbling

--- javascript/websimple.js
+++ javascript/websimple.js
@@ -1,20 +1,31 @@
 
 $(document).ready(function(){
 	var count=0;
-
-	//Mettre en pace les controles
-	$("section.page").on("chargerPanel", function(){
-		$(this).prepend('<aside class="control_box"><div class="move"></div></aside>');
-	});
-	$("section.page").trigger("chargerPanel"); 	
-	
 	//le statut par défaut
 	 if ($.cookie('move')) {
 	 	var statut=$.cookie('move');
 	 	}
 	 else{
 	 	var statut='on';
-	 	}
+	 	}	
+	
+	
+	//Mis en plus de la structure de fenêtres flottables
+	$(".flottable").each(function(){   
+   		count=count+1;
+   		$('aside.fenetres').append('<div class="fenetre_container" id="fenetre_'+count+'" data-fenetre="'+count+'"></div>');
+   		//donner l'attribut de la fenetre
+   		$(this).find( 'a.closed').each(function(){ 
+   			$(this).attr('data-fenetre',count);
+   		});
+	 });
+	//Mettre en pace les controles
+	$("section.page").on("chargerPanel", function(){
+		$(this).prepend('<aside class="control_box"><div class="move"></div></aside>');
+	});
+	$("section.page").trigger("chargerPanel"); 	
+	
+
 	 	
 	$('aside.control_box .move').addClass(statut);
 	 if(statut=="off"){
@@ -23,7 +34,7 @@ $(document).ready(function(){
 	 else{
 	 	moveOn();
 	 }
-
+	 
 	//Désactiver le draggable
 	$('.control_box').on('click','.off',function(){
 		moveOff();
@@ -33,71 +44,30 @@ $(document).ready(function(){
 	$('.control_box').on('click','.on',function(){
 		moveOn();
 	});
+	 
 
-	$(".options").on("chargerFenetre", function(e,id_article,id_article_base,statut,panier,faq){
-		e.stopPropagation();
-		if(statut=='closed'){
-			e.stopPropagation();
-			//préparer le cadre html
-	        $('#article_'+id_article+' div.load').replaceWith(
-	        '<div class="floating_box" id="floating_box_'+id_article+'" style="display:none;"><div class="panneau"><div class="action_close" id="close_'+id_article+'">X</div></div><div class="floating_content"> </div></div>');
-	        //charger le contenu
-	        $('#floating_box_'+id_article+' .floating_content').load(
-	            '/spip.php?action=charger_squelette&squelette=content/article-packs&id_article='+id_article+'&forum=non&id_article_base='+id_article_base+'&panier='+panier+'&faq='+faq,'',function(){
-	            	//mettre la fenêtre en avant
-	            	fenetreUp($('#article_'+id_article));
-	            	//mettre la fenêtre en avant faire apparaite la fenêtre et daer le html  
-	                $('#floating_box_'+id_article).show(800);
-	                $('#link_'+id_article+'_'+id_article_base+' span.close').replaceWith('<span class="open">-</span>');
-	                $('#link_'+id_article+'_'+id_article_base).removeClass("closed").addClass("open");
-					//rendre a fenêre resizable
-				    $( ".floating_box" ).resizable({ animateEasing: "easeOutBounce" });
-		
-	               // Fermer une fenetre via le x
-			        $('.action_close').click(function(){
-			        	$('.options').off("click", ".options","chargerFenetre");
-			        	e.stopPropagation();
-				            var id=$(this).attr('id').split('_');   
-				            var id_article=id[1];
-				           $('#floating_box_'+id_article).hide(800);
-				           $('#link_'+id_article+'_'+id_article_base+' span.open').replaceWith('<span class="close">+</span>');       
-				           $('#link_'+id_article+'_'+id_article_base).removeClass("open").addClass("closed");
-				        });
-				        
-	               // mettre la fenêtre active en avant
-			        $('.ui-draggable').click(function(){
-			        	fenetreUp($(this)); 
-				        });
-				      //acordeon  				        
-				   	$('dl.faq > dt').addClass("close").click(function(){
-				   		e.stopPropagation();
-						$(this).toggleClass("close").next().toggle('fast');
-						return false;
-						}).next().hide();    
-	             	}
-	             );
-	        }
-	     else{      
-		       	$('#floating_box_'+id_article).hide(800);
-		       	$('#link_'+id_article+'_'+id_article_base+' span.open').replaceWith('<span class="close">+</span>');       
-		       	$('#link_'+id_article+'_'+id_article_base).removeClass("open").addClass("closed");
-	        }
-	});
+});
+
+$(function() { 
 
 	//Ouvrir et fermer la fenêtre
 	      
-	$("dt").on("click","a",function (e) {
-		e.stopPropagation();
+	$(".options dt a").off("chargerFenetre").click(function(event) {
+		 event.stopImmediatePropagation();
 		var id=$(this).attr('id').split('_');   
 	    var id_article=id[1];
-	    var id_article_base=id[2];
+	    var id_article_base=$(this).attr('data-base');
 	    var statut=$(this).attr('class');
 	    var panier=$(this).attr('data-panier');   
-	    var faq=$(this).attr('data-faq');  
-	   $(".options").trigger("chargerFenetre",[id_article,id_article_base,statut,panier,faq]);  
+	    var faq=$(this).attr('data-faq'); 
+	    var fenetre=$(this).attr('data-fenetre');  
+	    chargerFenetre(id_article,id_article_base,statut,panier,faq,fenetre);
+	   //$(".flottable").trigger("chargerFenetre",[id_article,id_article_base,statut,panier,faq,fenetre]);  
 	   
 	   return false;
-	});
+	}); 	
+ });
+
 //Enlever l'effet move	
  function moveOff() {
 	var count=0;
@@ -126,24 +96,23 @@ $(document).ready(function(){
 	 }; 	
 // contrôle des fenêtres	
  function fenetreControle(statut,selector,count) {
+ 	
  	if(statut=='off'){
 		 $(selector).draggable( "disable" );
 		 $(selector).css({top:count*2+'%',left:0,opacity:1,position:'absolute'}); 		
  	}
  	else{
-		 $(function() {
-		        $(selector).draggable(
-		            {
-		            containment:'html', 
-		            opacity: 0.35, 
-		            cursor: 'move',
-		            start: function( event, ui ) {fenetreUp($(this));},    
-		            stop: function() {
-		                    $.cookie(selector,$(selector).css('top')+','+$(selector).css('left'), { expires: 365 , path: '/' });
-		                  }
-		             }
-		        );
-		      });
+        $(selector).draggable(
+            {
+            containment:'html', 
+            opacity: 0.35, 
+            cursor: 'move',
+            start: function( event, ui ) {fenetreUp($(this));},    
+            stop: function() {
+                    $.cookie(selector,$(selector).css('top')+','+$(selector).css('left'), { expires: 365 , path: '/' });
+                  }
+             }
+        );
 		$(selector).draggable( "enable" );
 		if ($.cookie(selector)) {
           var coords = $.cookie(selector).split(',');
@@ -159,5 +128,52 @@ $(document).ready(function(){
  function fenetreUp(elem) {
 	$('dl dd').css({'z-index':1000})
 	elem.css({'z-index':10001});
-    };     
-});
+    };  
+    
+ function chargerFenetre(id_article,id_article_base,statut,panier,faq,fenetre) {
+	if(statut=='closed'){	    
+		//préparer le cadre html
+        $('#fenetre_'+fenetre).append(
+        '<div class="floating_box" id="floating_box_'+id_article+'" style="display:none;"><div class="panneau"><div class="action_close" id="close_'+id_article+'">X</div></div><div class="floating_content"> </div></div>');
+        //charger le contenu
+        $('#floating_box_'+id_article+' .floating_content').load(
+            '/spip.php?action=charger_squelette&squelette=content/article-packs&id_article='+id_article+'&forum=non&id_article_base='+id_article_base+'&panier='+panier+'&faq='+faq,'',function(){
+            	//le rendre draggable
+            	fenetreControle('on','#floating_box_'+id_article);
+            	//mettre la fenêtre en avant
+            	fenetreUp($('#floating_box_'+id_article));
+            	//mettre la fenêtre en avant faire apparaite la fenêtre et daer le html  
+                $('#floating_box_'+id_article).show(800);
+                $('#link_'+id_article+' span.close').replaceWith('<span class="open">-</span>');
+                $('#link_'+id_article).removeClass("closed").addClass("open");
+				//rendre a fenêre resizable
+			    $( ".floating_box" ).resizable({ animateEasing: "easeOutBounce" });
+               // Fermer une fenetre via le x
+		        $('.action_close').click(function(){
+			            var id=$(this).attr('id').split('_');   
+			            var id_article=id[1];
+			           $('#floating_box_'+id_article).hide(800);
+			           $('#link_'+id_article+' span.open').replaceWith('<span class="close">+</span>');       
+			           $('#link_'+id_article).removeClass("open").addClass("closed");
+			        });
+			        
+               // mettre la fenêtre active en avant
+		        $('.ui-draggable').click(function(){
+		        	fenetreUp($(this)); 
+			        });
+			      //acordeon  				        
+			   	$('dl.faq > dt').addClass("close").click(function(){
+					$(this).toggleClass("close").next().toggle('fast');
+					return false;
+					}).next().hide();    
+             	}
+             	
+             );
+        }
+     else{      
+	       	$('#floating_box_'+id_article).hide(800);
+	       	$('#link_'+id_article+' span.open').replaceWith('<span class="close">+</span>');       
+	       	$('#link_'+id_article).removeClass("open").addClass("closed");
+        }
+    };  
+        	
