02e719c9402fbc7a236e497a39f6d2fb52db91fb
Rainer Müller <rainer@websimple.be>
2013-05-06 17:51:30 +0200

    inclure manquant

--- theme_websimple_administrations.php
+++ theme_websimple_administrations.php
@@ -2,12 +2,12 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/cextras');
-include_spip('base/titrecourt');
+include_spip('base/theme_websimple');
     
 function theme_websimple_upgrade($nom_meta_base_version,$version_cible) {
 
   $maj = array();
-  cextras_api_upgrade(theme_websimple_declarer_champs_extras(), $maj['create']); 
+  cextras_api_upgrade(theme_websimple_declarer_champs_extras(), $maj['0..2']); 
 
   include_spip('base/upgrade');
   maj_plugin($nom_meta_base_version, $version_cible, $maj);
