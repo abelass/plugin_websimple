8c5919d017005ed668e46c9ca69c4a75890189d4
Rainer Müller <rainer@websimple.be>
2013-05-07 09:00:10 +0200

    indications correctes

--- theme_websimple_administrations.php
+++ theme_websimple_administrations.php
@@ -7,7 +7,8 @@ include_spip('base/theme_websimple');
 function theme_websimple_upgrade($nom_meta_base_version,$version_cible) {
 
   $maj = array();
-  cextras_api_upgrade(theme_websimple_declarer_champs_extras(), $maj['0..2']); 
+  cextras_api_upgrade(theme_websimple_declarer_champs_extras(), $maj['create']); 
+  cextras_api_upgrade(theme_websimple_declarer_champs_extras(), $maj['0.0.2']); 
 
   include_spip('base/upgrade');
   maj_plugin($nom_meta_base_version, $version_cible, $maj);
