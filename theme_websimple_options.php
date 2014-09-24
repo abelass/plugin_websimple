cb23cf9b8095f368422bf44854abc859b0a388ad
Rainer Müller <rainer@websimple.be>
2014-09-24 10:07:07 +0200

    enklever la redirection pour eviter de problèmes avec le certifica ssl de piwik

--- theme_websimple_options.php
+++ theme_websimple_options.php
@@ -3,12 +3,11 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 // Le traffic HTTP est redirige en HTTPS
 
-if (
+/*if (
         (!$_SERVER['HTTPS']) AND
         ($_SERVER['REQUEST_METHOD'] == 'GET')
         ) {
         include_spip('inc/headers');
         redirige_par_entete('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-  echo 'ok';
-}
+}*/
 ?>
